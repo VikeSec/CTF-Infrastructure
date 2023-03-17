3795fd8180452bbcbd1e935f35700acbbcc4863b
Max Thomson <git@mnt.dev>
2023-03-16 20:06:08 -0700

    Add Kubeconfig download

--- src/main.tf
+++ src/main.tf
@@ -5,7 +5,25 @@ module "infra" {
   AWS_REGION = var.AWS_REGION
 }
 
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = <<EOT
+ssh -o StrictHostKeyChecking=no ubuntu@${data.aws_instances.masters.public_ips[0]} sudo chmod 666 /etc/rancher/k3s/k3s.yaml
+
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${data.aws_instances.masters.public_ips[0]}:/etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+ssh -o StrictHostKeyChecking=no ubuntu@${data.aws_instances.masters.public_ips[0]} sudo chmod 600 /etc/rancher/k3s/k3s.yaml
+
+EOT
+  }
+}
+
 module "k8s" {
   count  = 1
   source = "./k8s"
+}
+
+module "challs" {
+  count  = 1
+  source = "./challs"
 }
